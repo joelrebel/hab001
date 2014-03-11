b972c8e4522c11eb1648bc41e3e870822bd82cd5
Joel Rebello <joel.rebell@gmail.com>
2014-03-11 07:28:16 +0530

    enable GPGSV, GPRMC messages

--- lib/parse_nmea.h
+++ lib/parse_nmea.h
@@ -7,12 +7,12 @@ typedef struct {                                    // GPS Parse Info Structure
 } TGPI;                                             //
                                                     //
 static const TGPI gpi[] = {                         //
-    { "GPRMC",    0x03FE },                         //
-//    { "GPGGA",    0x007E },                         //
-
-    { "GPGGA",    0x0FFF },                         //setting this to the equivalent bits enables parsing for those bits
-    { "GPGLL",    0x005E },
-    { 0,          1      }                          //
+   { "GPRMC",    0x03FE },                         //
+   { "GPGSV",    0x0FFF },                         //
+   { "GPGGA",    0x0FFF },                         //setting this to the equivalent bits enables parsing for those bits
+//	 {"GPVTG", 0x3FE},
+//    { "GPGLL",    0x005E },
+    { 0,          3      }                          //This says, parse x sentences (2 if we want to parse GPGGA and GPVTG)
 };                                                  //
 
 static uint16_t hex(const uint8_t c);
